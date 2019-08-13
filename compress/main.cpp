ce3a284a7cd408fcdd434b4d8d32c424644455eb
Cody Li <licody2000@gmail.com>
2019-08-12 21:43:56 -0700

    commenting

--- compress/main.cpp
+++ compress/main.cpp
@@ -3,7 +3,9 @@
  * Author: Cody Li            
  * Date: May 2018
  * Email: licody2000@gmail.com
- * Purpose: Reduce the size of a .txt file by 1/8
+ * Purpose: Takes a .txt file and turns it into a binary tree with characters of the file as the leaves. 
+ * Then creates a path by going through the tree.  Next creates a hashtable with the paths and turns the 
+ * .txt file into binary. Finally turns the binary into ascii and writes the new ascii to a .txt file
  */
 #include <iostream>
 #include <fstream>
